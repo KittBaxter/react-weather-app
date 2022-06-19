728378fb08a3fe00a73858904515d98c601b83d4
KittBaxter <ladykitty826@gmail.com>
2022-06-19 13:39:11 -0400

    corrected some missplelling

--- src/Weather.js
+++ src/Weather.js
@@ -32,7 +32,7 @@ export default function Weather(props) {
   }
 
   function search() {
-    const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    const apiKey = "Acfdf9ae24166e7db0290ce2fa70a651";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
